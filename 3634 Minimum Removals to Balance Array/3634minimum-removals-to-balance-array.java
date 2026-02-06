c624e0c5cffdc8333346323db0bea708fcc3f1ea
Shashank <onlyshashankshekhar2007@gmail.com>
2026-02-06 14:13:32 +0000

    Code challenge submission for question: 3634 Minimum Removals to Balance Array

---
+++ 3634 Minimum Removals to Balance Array/3634minimum-removals-to-balance-array.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class Solution {
+    public int minRemoval(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        int n = nums.length;
+        int left = 0;
+        int maxLen = 1;
+
+        for (int right = 0; right < n; right++) {
+            while ((long) nums[right] > (long) k * nums[left]) {
+                left++;
+            }
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return n - maxLen;
+    }
+}
+
+
