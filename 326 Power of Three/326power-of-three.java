e34eef0b75c08c20d20e14c4a93ddb775299fdb0
Shashank <onlyshashankshekhar2007@gmail.com>
2026-02-26 14:16:05 +0000

    Code challenge submission for question: 326 Power of Three

---
+++ 326 Power of Three/326power-of-three.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        for(int i=0;i<=19;i++){
+            if(n == Math.pow(3,i))
+            return true;
+        }
+        return false;
+    }
+}
