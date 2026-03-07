95fe122560ccc6fc2ae5bc48772e77bb19f68b3a
Shashank <onlyshashankshekhar2007@gmail.com>
2026-03-07 05:11:56 +0000

    Code challenge submission for question: 2180 Count Integers With Even Digit Sum

---
+++ 2180 Count Integers With Even Digit Sum/2180count-integers-with-even-digit-sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countEven(int num) {
+        int count = 0;
+        for (int i = 1; i <= num; i++) {
+            int x = i;
+            int sum = 0;
+            while (x > 0) {
+
+                int a = x % 10;
+                sum += a;
+                x /= 10;
+            }
+            if (sum % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
