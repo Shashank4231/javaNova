1257bcdd012a622a35405c7f95e0ef434787f504
Shashank <onlyshashankshekhar2007@gmail.com>
2026-03-21 14:03:40 +0000

    Code challenge submission for question: 461 Hamming Distance

---
+++ 461 Hamming Distance/461hamming-distance.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+          int a = x ^ y;
+          int count = 0;
+          while(a>0){
+            a &= (a-1);
+            count ++;
+          }
+          return count;
+    }
+}
