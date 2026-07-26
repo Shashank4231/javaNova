9dec9751fca16d15e0076e68bcea9cc77a99fdad
Shashank <onlyshashankshekhar2007@gmail.com>
2026-07-26 12:38:07 +0000

    Code challenge submission for question: 402 Remove K Digits

---
+++ 402 Remove K Digits/402remove-k-digits.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String removeKdigits(String num, int k) {
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < num.length(); i++) {
+            while (!st.empty() && k > 0 && st.peek() > num.charAt(i)) {
+                st.pop();
+                k--;
+            }
+            st.push(num.charAt(i));
+        }
+
+        while (k > 0 && !st.empty()) {
+            st.pop();
+            k--;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while (!st.empty()) {
+            sb.append(st.pop());
+        }
+
+        sb.reverse();
+
+        while (sb.length() > 0 && sb.charAt(0) == '0') {
+            sb.deleteCharAt(0);
+        }
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
+}
